6274f78ed16a21097401627b14a61e78c21631f8
EC2 Default User <ec2-user@ip-172-31-0-107.ec2.internal>
2022-12-16 22:28:43 +0000

    Commiting Output of global vars with the allowed ips for security group

--- modules/globalvars/output.tf
+++ modules/globalvars/output.tf
@@ -17,6 +17,19 @@ data "http" "myip" {
   url = "http://ipv4.icanhazip.com"
 }
 
-output "public_ip" {
+output "public_ip_cloud9" {
   value = ["${chomp(data.http.myip.body)}/32"]
 }
+
+data "http" "mypriip" {
+  url = "http://169.254.169.254/latest/meta-data/local-ipv4"
+  }
+  
+output "private_cloud9_ip" {
+   value =  ["${chomp(data.http.mypriip.body)}/32"]
+   }
+   
+output "my_system_ip" {
+  value = "174.91.172.73/32"
+}   
+   
