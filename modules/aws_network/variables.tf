417480bcdd9e2c3a3519ab973e2bd5294dbed3c7
EC2 Default User <ec2-user@ip-172-31-0-107.ec2.internal>
2022-12-16 19:20:41 +0000

    making changes on modules and dev environments

--- modules/aws_network/variables.tf
+++ modules/aws_network/variables.tf
@@ -14,26 +14,26 @@ variable "prefix" {
 
 # Provision public subnets in custom VPC
 variable "public_cidr_blocks" {
-  default     = ["10.2.0.0/24", "10.2.1.0/24", "10.2.2.0/24"]
+  default     = []
   type        = list(string)
   description = "Public Subnet CIDRs"
 }
 
 variable "private_cidr_blocks" {
-  default     = ["10.2.3.0/24", "10.2.4.0/24", "10.2.5.0/24"]
+  default     = []
   type        = list(string)
   description = "Private Subnet CIDRs"
 }
 # VPC CIDR range
 variable "vpc_cidr" {
-  default     = "10.2.0.0/16"
+ # default     = "10.2.0.0/16"
   type        = string
   description = "VPC to host static web site"
 }
 
 # Variable to signal the current environment 
-variable "gname" {
-  default     = "ACSFINAL"
+variable "env" {
+  default     = "dev"
   type        = string
   description = "Deployment Environment"
 }
