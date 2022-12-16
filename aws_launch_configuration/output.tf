b00caed5d26a91bd3bcb804c407ffb06a5f46574
EC2 Default User <ec2-user@ip-172-31-0-107.ec2.internal>
2022-12-16 18:24:34 +0000

    changing the cloud9 environment

--- aws_launch_configuration/output.tf
+++
@@ -1,5 +0,0 @@
-#LaunchConfiguration_NAme
-output "Launch_Configuration _Name" {
-  value= aws_launch_configuration.launch_configuration.name
-}
-
--- aws_network/output.tf
+++
@@ -1,8 +0,0 @@
-# Add output variables
-output "public_subnet_ids" {
-  value = aws_subnet.public_subnet[*].id
-}
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
