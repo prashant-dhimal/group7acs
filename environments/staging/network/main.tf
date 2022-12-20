3e1274294c4ae0b08bca034068413f790e9caa38
prashant-dhimal <ec2-user@ip-172-31-0-107.ec2.internal>
2022-12-20 01:37:28 +0000

    updating network file of staging environment

---
+++ environments/staging/network/main.tf
@@ -0,0 +1,22 @@
+module "vpc-dev" {
+  source = "../../../modules/aws_network"
+  env                 = var.env
+  vpc_cidr            = var.vpc_cidr
+  private_cidr_blocks = var.private_subnet_cidrs
+  prefix              = var.prefix
+  default_tags        = var.default_tags
+  public_cidr_blocks  = var.public_subnet_cidrs
+
+}
+
+# Global Variable module
+module "global_variable" {
+  source = "../../../modules/globalvars"
+}
+
+#COnfiguring tags with global variable 
+locals {
+  default_tags = merge(module.global_variable.default_tags, { "env" = var.env })
+  prefix       = module.global_variable.prefix
+  name_prefix  = "${local.prefix}-${var.env}"
+}
---
+++ environments/staging/network/output.tf
@@ -0,0 +1,11 @@
+output "public_subnet_ids" {
+  value = module.vpc-dev.public_subnet_ids
+}
+
+output "vpc_id" {
+  value = module.vpc-dev.vpc_id
+}
+
+output "private_subnet_ids" {
+  value = module.vpc-dev.private_subnet_ids
+}
