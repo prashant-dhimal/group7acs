02b39acc1e7706127ad5b5a1dc5b21a1e464f7a4
prashant-dhimal <ec2-user@ip-172-31-0-107.ec2.internal>
2022-12-20 21:29:32 +0000

    Adding Network on Staging files

---
+++ environments/staging/network/output.tf
@@ -0,0 +1,11 @@
+output "public_subnet_ids" {
+  value = module.vpc-staging.public_subnet_ids
+}
+
+output "vpc_id" {
+  value = module.vpc-staging.vpc_id
+}
+
+output "private_subnet_ids" {
+  value = module.vpc-staging.private_subnet_ids
+}
