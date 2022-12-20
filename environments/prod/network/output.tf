cc07fc49765639441f28e4bd034418412edb14dd
Prince Thakuri <mrdeft1997@gmail.com>
2022-12-19 22:21:19 -0500

    updating output.tf and variables.tf

--- environments/prod/network/output.tf
+++ environments/prod/network/output.tf
@@ -1,11 +1,11 @@
 output "public_subnet_ids" {
-  value = module.vpc-dev.public_subnet_ids
+  value = module.vpc-prod.public_subnet_ids
 }
 
 output "vpc_id" {
-  value = module.vpc-dev.vpc_id
+  value = module.vpc-prod.vpc_id
 }
 
 output "private_subnet_ids" {
-  value = module.vpc-dev.private_subnet_ids
+  value = module.vpc-prod.private_subnet_ids
 }
--- environments/prod/network/variables.tf
+++ environments/prod/network/variables.tf
@@ -17,25 +17,25 @@ variable "prefix" {
 
 # Provision public subnets in custom VPC
 variable "public_subnet_cidrs" {
-  default     = ["10.200.0.0/24", "10.200.1.0/24", "10.200.2.0/24"]
+  default     = ["10.250.0.0/24", "10.250.1.0/24", "10.250.2.0/24"]
   type        = list(string)
   description = "Public Subnet CIDRs"
 }
 variable "private_subnet_cidrs" {
-  default     = ["10.200.3.0/24", "10.200.4.0/24", "10.200.5.0/24"]
+  default     = ["10.250.3.0/24", "10.250.4.0/24", "10.250.5.0/24"]
   description = "Private Subnet CIDRs"
 
 }
 # VPC CIDR range
 variable "vpc_cidr" {
-  default     = "10.200.0.0/16"
+  default     = "10.250.0.0/16"
   type        = string
   description = "VPC to host static web site"
 }
 
 # Variable to signal the current environment 
 variable "env" {
-  default     = "staging"
+  default     = "prod"
   type        = string
   description = "Deployment Environment"
 }
