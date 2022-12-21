a05da2b97d39cdb2e35a28e64f2c4ebd68e150ca
SubhamChetry <ec2-user@ip-172-31-72-86.ec2.internal>
2022-12-21 19:54:35 +0000

    Added aws_network module on Branch Staging

---
+++ modules/aws_network/main.tf
@@ -0,0 +1,123 @@
+# Step 1 - Define the provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Data source for availability zones in us-east-1
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Local variables
+locals {
+  default_tags = merge(
+    var.default_tags,
+    { "Env" = var.env }
+  )
+  name_prefix = "${var.prefix}-${var.env}"
+}
+
+# Create a new VPC 
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+  tags = merge(
+    local.default_tags, {
+      Name = "${local.name_prefix}-vpc"
+    }
+  )
+}
+
+
+# Add provisioning of the public subnetin the default VPC
+resource "aws_subnet" "public_subnet" {
+  count             = length(var.public_cidr_blocks)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_cidr_blocks[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index + 1]
+  tags = merge(
+    local.default_tags, {
+      Name = "${local.name_prefix}-public-subnet-${count.index}"
+    }
+  )
+}
+
+# Create Private Subnet
+resource "aws_subnet" "private_subnet" {
+  count             = length(var.private_cidr_blocks)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_cidr_blocks[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index + 1]
+  tags = merge(
+    local.default_tags, {
+      Name = "${local.name_prefix}-private-subnet-${count.index}"
+    }
+  )
+}
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-igw"
+    }
+  )
+}
+
+# Route table to route add default gateway pointing to Internet Gateway (IGW)
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${local.name_prefix}-route-public-route_table"
+  }
+}
+
+# Associate subnets with the custom route table
+resource "aws_route_table_association" "public_route_table_association" {
+  count          = length(aws_subnet.public_subnet[*].id)
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+}
+
+#Elastic IP for Natgateway 
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+}
+
+#Natgateway for Private Subnet
+resource "aws_nat_gateway" "natgateway" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = "${aws_subnet.public_subnet[1].id}"
+  tags = {
+    Name = "${local.name_prefix}-NAT-GATEWAY"
+  }
+
+  # Dependency of NATWAY
+  depends_on = [aws_internet_gateway.igw]
+}
+
+#Route_Table_for Private_Subnet
+# Private routes
+resource "aws_route_table" "private_subnet_route" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgateway.id
+  }
+
+  tags = {
+    Name = "${local.name_prefix}--private-route_table"
+  }
+}
+
+resource "aws_route_table_association" "private-route-table-association" {
+  count          = length(aws_subnet.private_subnet[*].id)
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_subnet_route.id
+}
